704ff7292554a6bee8917f8a8abab5c5123f6d83
ErolKocoglu <erolkocoglu44@gmail.com>
2026-06-13 23:53:40 +0300

    bff and auth added

---
+++ microservices/auth-service/src/main/java/com/turkcell/auth/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.turkcell.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .formLogin(formLogin -> formLogin.permitAll());
+
+        return http.build();
+    }
+}
